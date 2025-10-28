af78e2c527856190ff378a00bfece646eaec4b2b
maxime-fourquie <maxime.fourquie@laplateforme.io>
2025-10-28 09:57:23 +0100

    ai-lvl0-working

--- vscode/TicTacToe/TicTacToe.py
+++ vscode/TicTacToe/TicTacToe.py
@@ -30,106 +30,124 @@ board=[variable_vide,variable_vide,variable_vide,variable_vide,variable_vide,var
 signe_joueur="O"
 signe="X"
 choix_IA=0
-#laisser le choix du signe ?
+#laisser le choix du signe ? TO DO lat8
 
 #choix du mode jeu
 #2joueurs ou ia
-mode_de_jeu=input("Choix du mode de jeu : 2 Joueurs / VS IA:")
+# input("TiTacToe \n mode de jeu :\n-2 Players\n-Vs AI") # mettre en anglais
+# input()
+# mode_de_jeu_reponse=input("want to play 2 players mode ? Y/N\n")
+# if mode_de_jeu_reponse=="Y": #creer variation de reponse
+#     print("loading 2 player mode")#load 2 player mode
 
-    #IA focntion
 
-def ia(board,signe): #simple ia
-    choix_IA=random.randrange(0,8)
-    while board[choix_IA]==signe or board[choix_IA]==signe_joueur:
-        choix_IA=random.randrange(0,8) #make an or with if
-    # print("choix ia dans la fonction:",choix_IA)
-    return choix_IA #emplacemnt
-ia(board,signe)
+
+
+
+
+# elif mode_de_jeu_reponse=="N":
+if True: #placeholder
+    print("loading AI mode...\n")
+    print("Template")
+    print(mur,"0",mur,"1",mur,"2",mur) 
+    print(mur,"3",mur,"4",mur,"5",mur)
+    print(mur,"6",mur,"7",mur,"8",mur)
+    print()
     
 
-# if tour>9:
-#     print("égalité")
-# else:
+        #IA focntion
 
-while tour <=9: #le jeu continu tant que 9 tour ne sont pas passé
-    #player1
+    def ia(board,signe): #simple ia
+        choix_IA=random.randrange(0,8)
+        while board[choix_IA]==signe or board[choix_IA]==signe_joueur:
+            choix_IA=random.randrange(0,8) #make an or with if
+        # print("choix ia dans la fonction:",choix_IA)
+        return choix_IA #emplacemnt
+    ia(board,signe)
+        
 
-    choix_joueur1=int(input("selectionner emplacement: ")) #ajouter template
-    while board[choix_joueur1]==signe or board[choix_joueur1]==signe_joueur:
-        print("emplacement occupé") # try again a faire
-        choix_joueur1=int(input("selectionner emplacement: "))
-
-    if board[choix_joueur1]==variable_vide: #ajouter exeption si choix du joueur est >9
-        board[choix_joueur1]=signe_joueur
-      
-    print("Tour :",tour, "Joueur1")
-    print(mur,board[0],mur,board[1],mur,board[2],mur) 
-    print(mur,board[3],mur,board[4],mur,board[5],mur)
-    print(mur,board[6],mur,board[7],mur,board[8],mur)
-
-    if (((board[0]==signe_joueur and board[1]==signe_joueur and board[2])==signe_joueur) or
-    ((board[3]==signe_joueur and board[4]==signe_joueur and board[5])==signe_joueur) or
-    ((board[6]==signe_joueur and board[7]==signe_joueur and board[8])==signe_joueur) or
-    ((board[0]==signe_joueur and board[3]==signe_joueur and board[6])==signe_joueur) or
-    ((board[1]==signe_joueur and board[4]==signe_joueur and board[7])==signe_joueur) or
-    ((board[2]==signe_joueur and board[5]==signe_joueur and board[8])==signe_joueur) or
-    ((board[0]==signe_joueur and board[4]==signe_joueur and board[8])==signe_joueur) or
-    ((board[2]==signe_joueur and board[4]==signe_joueur and board[6])==signe_joueur)):
-        print("Joueur1 win")
-        break
-    tour+=1
-
-        #IA temporaire
-    #choix_IA=random.randrange(0,8)       #valeur retourné par la fonction a changer
-    while board[choix_IA]==signe or board[choix_IA]==signe_joueur:
-        choix_IA=random.randrange(0,8) #make an or with if
-    # elif board[choix_IA]==signe_joueur:
-    #     choix_IA=random.randrange(0,8)  #trying for looping test, need to change with alogrythm
-    
-    board[choix_IA]=signe
-
-    print("Tour :",tour, "Ai choix :",choix_IA)
-    print(mur,board[0],mur,board[1],mur,board[2],mur)
-    print(mur,board[3],mur,board[4],mur,board[5],mur)
-    print(mur,board[6],mur,board[7],mur,board[8],mur)
-
-    if (((board[0]==signe and board[1]==signe and board[2])==signe) or
-    ((board[3]==signe and board[4]==signe and board[5])==signe) or
-    ((board[6]==signe and board[7]==signe and board[8])==signe) or
-    ((board[0]==signe and board[3]==signe and board[6])==signe) or
-    ((board[1]==signe and board[4]==signe and board[7])==signe) or
-    ((board[2]==signe and board[5]==signe and board[8])==signe) or
-    ((board[0]==signe and board[4]==signe and board[8])==signe) or
-    ((board[2]==signe and board[4]==signe and board[6])==signe)):
-        print("AI win")
-        break
-    tour+=1
-    # if tour==10:
+    # if tour>9:
     #     print("égalité")
-    #     break
+    # else:
+
+    while tour <=9: #le jeu continu tant que 9 tour ne sont pas passé
+        #player1
+        choix_joueur1=int(input("Your turn\nselectionner emplacement: ")) #ajouter template
+        while board[choix_joueur1]==signe or board[choix_joueur1]==signe_joueur:
+            print("emplacement occupé") # try again a faire
+            choix_joueur1=int(input("selectionner emplacement: "))
+
+        if board[choix_joueur1]==variable_vide: #ajouter exeption si choix du joueur est >9
+            board[choix_joueur1]=signe_joueur
+        print("________________________________________")
+        print()
+        print("Tour :",tour, "Joueur1")
+        print(mur,board[0],mur,board[1],mur,board[2],mur) 
+        print(mur,board[3],mur,board[4],mur,board[5],mur)
+        print(mur,board[6],mur,board[7],mur,board[8],mur)
+
+        if (((board[0]==signe_joueur and board[1]==signe_joueur and board[2])==signe_joueur) or
+        ((board[3]==signe_joueur and board[4]==signe_joueur and board[5])==signe_joueur) or
+        ((board[6]==signe_joueur and board[7]==signe_joueur and board[8])==signe_joueur) or
+        ((board[0]==signe_joueur and board[3]==signe_joueur and board[6])==signe_joueur) or
+        ((board[1]==signe_joueur and board[4]==signe_joueur and board[7])==signe_joueur) or
+        ((board[2]==signe_joueur and board[5]==signe_joueur and board[8])==signe_joueur) or
+        ((board[0]==signe_joueur and board[4]==signe_joueur and board[8])==signe_joueur) or
+        ((board[2]==signe_joueur and board[4]==signe_joueur and board[6])==signe_joueur)):
+            print("Joueur1 win")
+            break
+        tour+=1
+
+            #IA temporaire
+        #choix_IA=random.randrange(0,8)       #valeur retourné par la fonction a changer
+        while board[choix_IA]==signe or board[choix_IA]==signe_joueur:
+            choix_IA=random.randrange(0,8) #make an or with if
+        # elif board[choix_IA]==signe_joueur:
+        #     choix_IA=random.randrange(0,8)  #trying for looping test, need to change with alogrythm
+        
+        board[choix_IA]=signe
+
+        print("Tour :",tour, "Ai choix :",choix_IA)
+        print(mur,board[0],mur,board[1],mur,board[2],mur)
+        print(mur,board[3],mur,board[4],mur,board[5],mur)
+        print(mur,board[6],mur,board[7],mur,board[8],mur)
+
+        if (((board[0]==signe and board[1]==signe and board[2])==signe) or
+        ((board[3]==signe and board[4]==signe and board[5])==signe) or
+        ((board[6]==signe and board[7]==signe and board[8])==signe) or
+        ((board[0]==signe and board[3]==signe and board[6])==signe) or
+        ((board[1]==signe and board[4]==signe and board[7])==signe) or
+        ((board[2]==signe and board[5]==signe and board[8])==signe) or
+        ((board[0]==signe and board[4]==signe and board[8])==signe) or
+        ((board[2]==signe and board[4]==signe and board[6])==signe)):
+            print("AI win")
+            break
+        tour+=1
+        # if tour==10:
+        #     print("égalité")
+        #     break
+
+        # print("Tour:",tour)
+
+        #condition exeko a faire
 
 
-    # print("Tour:",tour)
+        # def plateau(n):
+        # print(mur,list_vide[0],mur,list_vide[1],mur,list_vide[2],mur)
+        # print(mur,list_vide[3],mur,list_vide[4],mur,list_vide[5],mur)
+        # print(mur,list_vide[6],mur,list_vide[7],mur,list_vide[8],mur)
+        # plateau(3)
 
-    #condition exeko a faire
-
-
-    # def plateau(n):
-    # print(mur,list_vide[0],mur,list_vide[1],mur,list_vide[2],mur)
-    # print(mur,list_vide[3],mur,list_vide[4],mur,list_vide[5],mur)
-    # print(mur,list_vide[6],mur,list_vide[7],mur,list_vide[8],mur)
-    # plateau(3)
-
-    #condition de victoire
-    #si board[0],1 ,2 == "X"
+        #condition de victoire
+        #si board[0],1 ,2 == "X"
 
 
 
-    
+        
 
-    #si //same =="O"
-        #joueur win
-    #si tour 9 and no winnder, draw
+        #si //same =="O"
+            #joueur win
+        #si tour 9 and no winnder, draw
 
 
 
